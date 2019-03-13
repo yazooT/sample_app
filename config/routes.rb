b2ae718e196c80e869d76d048298ca12a5589ddc
yazooT <ec2-user@ip-172-31-43-64.us-east-2.compute.internal>
2019-03-13 16:56:43 +0000

    正しいrootを設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#home'
+  root 'static_pages#home'
 end
