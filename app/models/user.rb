c516addc431d695b724f5e20f7fb6e884bf5bd4a
yazooT <ec2-user@ip-172-31-43-64.us-east-2.compute.internal>
2019-03-19 17:35:35 +0000

    emailをインデックス化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  
   validates :name, presence: true,
-                   length: { maximum: 50}
+                   length: { maximum: 50},
+                   uniqueness: { case_sensitive: false }
                    
   validates :email, presence: true,
-                    length: { maximum: 255}
+                    length: { maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,4 +30,29 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end 
   
+  test "email validation should not accept invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com hogehoge@example...com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end 
+  end 
+  
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com user.name@example.co.jp
+                           foo@barbaz.com foo@barbaz.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end 
+  end 
+  
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
 end
