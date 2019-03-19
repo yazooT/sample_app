70852496fbde42bc93cf6ccdd9f2c36ae7028b28
yazooT <ec2-user@ip-172-31-43-64.us-east-2.compute.internal>
2019-03-19 13:51:49 +0000

    DBのマイグレーションを追加、ユーザーモデルを追加、ユーザーに存在認証と長さ認証を追加

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true,
+                   length: { maximum: 50}
+                   
+  validates :email, presence: true,
+                    length: { maximum: 255}
+end
