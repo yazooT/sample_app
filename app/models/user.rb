d2b0f0eb545880ad4de9e83d9d258002ac04d343
yazooT <ec2-user@ip-172-31-43-64.us-east-2.compute.internal>
2019-03-19 17:43:28 +0000

    メールアドレスを全て小文字にして保存するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
+  before_save { self.email = self.email.downcase }
+  
   validates :name, presence: true,
                    length: { maximum: 50},
                    uniqueness: { case_sensitive: false }
