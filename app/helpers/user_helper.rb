2423ac3166729f4b03015f528d8c77b047dcf677
yazooT <ec2-user@ip-172-31-43-64.us-east-2.compute.internal>
2019-03-24 11:43:14 +0000

    grabatarからプロフィール画像を取得できるように変更

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,13 @@
 module UserHelper
+  
+  #引数で与えられたUserのイメージタグを返します
+  #画像はGravatarから取得します
+  def gravatar_for(user, size: 80 )
+    email = user.email.downcase
+    gravatar_id = Digest::MD5::hexdigest(email)
+    
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'user#new'
-
-  get '/help', to: 'static_pages#help'
-  
-  get '/about', to: 'static_pages#about'
-  
-  get '/contact', to: 'static_pages#contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  resources :users
 end
